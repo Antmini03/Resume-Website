ecd7a0f164e6562dd73d07b23eca6ac05d2aa7e3
Anthony Miniciello <aminichiello@law.harvard.edu>
2022-05-16 11:54:41 -0400

    setup express static scaffold

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
     res.send('Hello World! <br \> This should be on the second line.');
 });
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+app.listen(PORT, function() {
+    console.log(`Example app listening on port: ${PORT}!`);
 });
